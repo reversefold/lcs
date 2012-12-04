ea835b468a556488637a24dbf85bad755c6b0529
Justin Patrin <justin@idle-games.com>
2012-12-03 21:38:21 -0800

    Move bin-release to index

--- README.md
+++
@@ -1,3 +0,0 @@
-# Longest Common Substring #
-
-An implementation of the Longest Common Substring algorithm with support for varibale length codons (matching sets of characters).
--- python/util/__init__.py
+++
@@ -1,5 +0,0 @@
-def print_table(arr):
-    max_len = max(max(len(str(c)) for c in r) for r in arr)
-    row_format = ('{:>' + str(max_len + 1) + '}') * len(arr[0])
-    for y in xrange(len(arr)):
-        print row_format.format(*arr[y])

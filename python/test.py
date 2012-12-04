c2636c45e2fa9fc7964ac0c14ceaae5d36a36125
Justin Patrin <justin@idle-games.com>
2012-12-03 19:17:09 -0800

    Add .gitignore and switch to lcs_string for test

--- python/test.py
+++ python/test.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import random
 
-from lcs import lcs
+from lcs import lcs_string
 
 
 def main():
     LEN = 60
     a = ''.join(random.choice(('a', 'c', 'g', 't')) for i in xrange(LEN))
     b = ''.join(random.choice(('a', 'c', 'g', 't')) for i in xrange(LEN))
-    print lcs(a, b, 3, True)
+    print lcs_string(a, b, 3, True)
 
 
 if __name__ == '__main__':

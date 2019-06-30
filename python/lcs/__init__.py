af1ae3c0268d24f9cae65da9f3a81eb296edb8a5
Justin Patrin <papercrane@reversefold.com>
2019-06-29 23:58:29 -0500

    Python 3 compatibility

--- python/lcs/__init__.py
+++ python/lcs/__init__.py
@@ -8,63 +8,63 @@ def lcs_string(input_a, input_b, codon_length=1, verbose=False):
     input strings have spaces the algorithm will still run fine but
     the output will be ambiguous.
 
-    >>> print '%r\n%r\n%r' % lcs_string('aaa' * 10, 'aaa' * 10)
+    >>> print('%r\n%r\n%r' % lcs_string('aaa' * 10, 'aaa' * 10))
     'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     30
-    >>> print '%r\n%r\n%r' % lcs_string('abc' * 10, 'abc' * 10)
+    >>> print('%r\n%r\n%r' % lcs_string('abc' * 10, 'abc' * 10))
     'abcabcabcabcabcabcabcabcabcabc'
     'abcabcabcabcabcabcabcabcabcabc'
     30
-    >>> print '%r\n%r\n%r' % lcs_string('abc' * 10, 'cba' * 10)
+    >>> print('%r\n%r\n%r' % lcs_string('abc' * 10, 'cba' * 10))
     '  a b c a b c a b c abcabcabcabcabcabcabc'
     'cbacbacbacbacbacbacba c b a c b a c b a  '
     19
-    >>> print '%r\n%r\n%r' % lcs_string('abc' * 10, 'cab' * 10)
+    >>> print('%r\n%r\n%r' % lcs_string('abc' * 10, 'cab' * 10))
     ' abcabcabcabcabcabcabcabcabcabc'
     'cabcabcabcabcabcabcabcabcabcab '
     29
-    >>> print '%r\n%r\n%r' % lcs_string('aaa' * 10, 'aaa' * 10, 3)
+    >>> print('%r\n%r\n%r' % lcs_string('aaa' * 10, 'aaa' * 10, 3))
     'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
     10
-    >>> print '%r\n%r\n%r' % lcs_string('abc' * 10, 'abc' * 10, 3)
+    >>> print('%r\n%r\n%r' % lcs_string('abc' * 10, 'abc' * 10, 3))
     'abcabcabcabcabcabcabcabcabcabc'
     'abcabcabcabcabcabcabcabcabcabc'
     10
-    >>> print '%r\n%r\n%r' % lcs_string('abc' * 10, 'cba' * 10, 3)
+    >>> print('%r\n%r\n%r' % lcs_string('abc' * 10, 'cba' * 10, 3))
     '                              abcabcabcabcabcabcabcabcabcabc'
     'cbacbacbacbacbacbacbacbacbacba                              '
     0
-    >>> print '%r\n%r\n%r' % lcs_string('abc' * 10, 'cab' * 10, 3)
+    >>> print('%r\n%r\n%r' % lcs_string('abc' * 10, 'cab' * 10, 3))
     ' abcabcabcabcabcabcabcabcabc  abc'
     'cabcabcabcabcabcabcabcabcabcab   '
     9
-    >>> print '%r\n%r\n%r' % lcs_string('d' + ('abc' * 10), 'e' + ('abc' * 10), 3)
+    >>> print('%r\n%r\n%r' % lcs_string('d' + ('abc' * 10), 'e' + ('abc' * 10), 3))
     ' dabcabcabcabcabcabcabcabcabcabc'
     'e abcabcabcabcabcabcabcabcabcabc'
     10
-    >>> print '%r\n%r\n%r' % lcs_string(('abc' * 10) + 'd', ('abc' * 10) + 'e', 3)
+    >>> print('%r\n%r\n%r' % lcs_string(('abc' * 10) + 'd', ('abc' * 10) + 'e', 3))
     'abcabcabcabcabcabcabcabcabcabc d'
     'abcabcabcabcabcabcabcabcabcabce '
     10
-    >>> print '%r\n%r\n%r' % lcs_string(('abc' * 10) + 'de', ('abc' * 10) + 'ed', 3)
+    >>> print('%r\n%r\n%r' % lcs_string(('abc' * 10) + 'de', ('abc' * 10) + 'ed', 3))
     'abcabcabcabcabcabcabcabcabcabc  de'
     'abcabcabcabcabcabcabcabcabcabced  '
     10
-    >>> print '%r\n%r\n%r' % lcs_string('e' + ('abc' * 10) + 'd', 'd' + ('abc' * 10) + 'e', 3)
+    >>> print('%r\n%r\n%r' % lcs_string('e' + ('abc' * 10) + 'd', 'd' + ('abc' * 10) + 'e', 3))
     ' eabcabcabcabcabcabcabcabcabcabc d'
     'd abcabcabcabcabcabcabcabcabcabce '
     10
-    >>> print '%r\n%r\n%r' % lcs_string('eee' + ('abc' * 10) + 'ddd', 'ddd' + ('abc' * 10) + 'eee', 3)
+    >>> print('%r\n%r\n%r' % lcs_string('eee' + ('abc' * 10) + 'ddd', 'ddd' + ('abc' * 10) + 'eee', 3))
     '   eeeabcabcabcabcabcabcabcabcabcabc   ddd'
     'ddd   abcabcabcabcabcabcabcabcabcabceee   '
     10
-    >>> print '%r\n%r\n%r' % lcs_string('eee' + ('abc' * 10) + 'ddd', 'ddd' + ('abc' * 5) + 'eee', 3)
+    >>> print('%r\n%r\n%r' % lcs_string('eee' + ('abc' * 10) + 'ddd', 'ddd' + ('abc' * 5) + 'eee', 3))
     '   eeeabcabcabcabcabc   abcabcabcabcabcddd'
     'ddd   abcabcabcabcabceee                  '
     5
-    >>> print '%r\n%r\n%r' % lcs_string('eee' + ('abc' * 5) + 'ddd', 'ddd' + ('abc' * 10) + 'eee', 3)
+    >>> print('%r\n%r\n%r' % lcs_string('eee' + ('abc' * 5) + 'ddd', 'ddd' + ('abc' * 10) + 'eee', 3))
     '   eeeabcabcabcabcabc                  ddd'
     'ddd   abcabcabcabcabcabcabcabcabcabceee   '
     5
@@ -78,23 +78,23 @@ def lcs_string(input_a, input_b, codon_length=1, verbose=False):
 def lcs(input_a, input_b, codon_length=1, verbose=False):
     r"""Longest Common Substring with variable codon length.
 
-    >>> print '%r\n%r\n%r' % lcs((0, 1, 2) * 3, (2, 0, 1) * 3)
+    >>> print('%r\n%r\n%r' % lcs((0, 1, 2) * 3, (2, 0, 1) * 3))
     [None, 0, 1, 2, 0, 1, 2, 0, 1, 2]
     [2, 0, 1, 2, 0, 1, 2, 0, 1, None]
     8
-    >>> print '%r\n%r\n%r' % lcs((0, 1, 2) * 3, (2, 0, 1) * 3, 3)
+    >>> print('%r\n%r\n%r' % lcs((0, 1, 2) * 3, (2, 0, 1) * 3, 3))
     [None, 0, 1, 2, 0, 1, 2, None, None, 0, 1, 2]
     [2, 0, 1, 2, 0, 1, 2, 0, 1, None, None, None]
     2
-    >>> print '%r\n%r\n%r' % lcs((0, 1, 2) * 3, (0, 1, 2) * 3, 3)
+    >>> print('%r\n%r\n%r' % lcs((0, 1, 2) * 3, (0, 1, 2) * 3, 3))
     [0, 1, 2, 0, 1, 2, 0, 1, 2]
     [0, 1, 2, 0, 1, 2, 0, 1, 2]
     3
-    >>> print '%r\n%r\n%r' % lcs((0, 1, 2) * 3, (0, 1, 2) * 3)
+    >>> print('%r\n%r\n%r' % lcs((0, 1, 2) * 3, (0, 1, 2) * 3))
     [0, 1, 2, 0, 1, 2, 0, 1, 2]
     [0, 1, 2, 0, 1, 2, 0, 1, 2]
     9
-    >>> print '%r\n%r\n%r' % lcs((0, 1, 2) * 3, (2, 1, 0) * 3)
+    >>> print('%r\n%r\n%r' % lcs((0, 1, 2) * 3, (2, 1, 0) * 3))
     [None, None, 0, None, 1, None, 2, 0, 1, 2, 0, 1, 2]
     [2, 1, 0, 2, 1, 0, 2, None, 1, None, 0, None, None]
     5
@@ -102,14 +102,14 @@ def lcs(input_a, input_b, codon_length=1, verbose=False):
 
     if verbose:
         from util import print_table
-        print input_a
-        print input_b
-    match_matrix = [[0] * (len(input_b) + 1) for i in xrange(len(input_a) + 1)]
+        print(input_a)
+        print(input_b)
+    match_matrix = [[0] * (len(input_b) + 1) for i in range(len(input_a) + 1)]
     if verbose:
-        matching_points = [[''] * (len(input_b) + 1) for i in xrange(len(input_a) + 1)]
+        matching_points = [[''] * (len(input_b) + 1) for i in range(len(input_a) + 1)]
     codon_length_less_1 = codon_length - 1
-    for x in xrange(codon_length - 1, len(input_a)):
-        for y in xrange(codon_length - 1, len(input_b)):
+    for x in range(codon_length - 1, len(input_a)):
+        for y in range(codon_length - 1, len(input_b)):
             match_matrix[x + 1][y + 1] = max(
                 ((1 if input_a[x - codon_length_less_1:x + 1] == input_b[y - codon_length_less_1:y + 1]
                   else 0)
@@ -120,14 +120,13 @@ def lcs(input_a, input_b, codon_length=1, verbose=False):
                 if match_matrix[x + 1][y + 1] != match_matrix[x][y + 1] and match_matrix[x + 1][y + 1] != match_matrix[x + 1][y]:
                     matching_points[x + 1][y + 1] = 'x'
 
-
     if verbose:
         display_matrix = match_matrix[:]
-        for i in xrange(len(display_matrix)):
+        for i in range(len(display_matrix)):
             display_matrix[i] = display_matrix[i][:]
         display_matrix[0] = [''] + list(input_b)
         matching_points[0] = [''] + list(input_b)
-        for x in xrange(len(input_a)):
+        for x in range(len(input_a)):
             display_matrix[x + 1][0] = input_a[x]
             matching_points[x + 1][0] = input_a[x]
         print_table(display_matrix)
@@ -164,13 +163,13 @@ def lcs(input_a, input_b, codon_length=1, verbose=False):
     result_b.reverse()
     if verbose:
         if isinstance(input_a, str):
-            print ''.join(' ' if c is None else c for c in result_a)
+            print(''.join(' ' if c is None else c for c in result_a))
         else:
-            print result_a
+            print(result_a)
         if isinstance(input_b, str):
-            print ''.join(' ' if c is None else c for c in result_b)
+            print(''.join(' ' if c is None else c for c in result_b))
         else:
-            print result_b
+            print(result_b)
     return (result_a, result_b, match_matrix[len(input_a)][len(input_b)])
 
 
--- python/test.py
+++ python/test.py
@@ -6,9 +6,9 @@ from lcs import lcs_string
 
 def main():
     LEN = 60
-    a = ''.join(random.choice(('a', 'c', 'g', 't')) for i in xrange(LEN))
-    b = ''.join(random.choice(('a', 'c', 'g', 't')) for i in xrange(LEN))
-    print lcs_string(a, b, 3, True)
+    a = ''.join(random.choice(('a', 'c', 'g', 't')) for i in range(LEN))
+    b = ''.join(random.choice(('a', 'c', 'g', 't')) for i in range(LEN))
+    print(lcs_string(a, b, 3, True))
 
 
 if __name__ == '__main__':
